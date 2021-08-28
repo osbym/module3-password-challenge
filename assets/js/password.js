101bb63421313052eca19eb5ca095d9eaf5f83ef
osbym <marosb79@yahoo.com>
2021-08-28 17:58:25 -0500

    added global variables

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,5 +1,14 @@
 // Assignment code here
 
+// Global Variables
+// When asked for lower, upper, numeric and character types to include in the password//
+
+var lowercaseOptions = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseOptions = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericOptions= "0123456789";
+var specialOptions = "!@#$%&?*+=";
+// Acceptance Criteria - GIVEN I need a new, secure password
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +24,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
