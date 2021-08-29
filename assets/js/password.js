2838dbe824eaf2967c9b38ebe183615715bc98eb
osbym <marosb79@yahoo.com>
2021-08-29 01:44:14 -0500

    concat back to the array for upper/lower/numeric

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,8 +1,7 @@
 // Assignment code here - Password Generator
 
-
-// Declare variables or arrays below.
 // When asked for lower, upper, numeric and character types to include in the password//
+//Data needed to store the arrays
 
 let lowercaseOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let uppercaseOptions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,9 +16,10 @@ let passArray = [];
 // Taken from Source Code
 var generateBtn = document.querySelector("#generate");
 let passwordChar = document.querySelector("password");
+let passwordDisplay = document.getElementById("passwordDisplay");
 
 //WHEN prompted for the length of the password THEN I choose a length between 8 and 128 characters
-function generatePass() {
+function generatePassword () {
     let charLength = prompt("Please choose a password length between 8 and 128 characters.");
 }
     // ALERT - "Plese choose password between 8 and 128 characters"
@@ -27,82 +27,50 @@ function generatePass() {
     if (charLength >= 8 && charLength <= 128) {
         passArray = [];
         generatedPass = [];
-    }
+
+
+//CONFIRM whether or not to include lower, upper, numeric, and/or special characters
+//WHEN I answer each prompt THEN MY input should be validated and at least one character type is selected
+let incLowerCaseOptions = confirm("Want to include lowercase characters in your password? Select OK for YES and CANCEL for NO");
+let incUpperCaseOptions = confirm("Want to include uppercase characters in your password? Select OK for YES and CANCEL for NO");
+let incNumericOptions = confirm("Want to include numbers in your password? Select OK for YES and CANCEL for NO");
+let incSpecialOptions = confirm("Want to include special characters in your password? Select OK for YES and CANCEL for NO");
+
+//CONFIRM what the user as selected - CONCAT back to the array
+if (incLowerCaseOptions === true) {
+    passArray = passArray.concat(lowercaseoptions);
+}
+if (incUpperCaseOptions === true) {
+    passArray = passArray.concat(uppercaseoptions);
+}
+if (incNumericOptions === true) {
+    passArray = passArray.concat(numericoptions);
+}
+if (incSpecialOptions === true) {
+    passArray = passArray.concat(specialoptions);
+}
+
+    
 
 //WHEN User enters nothing OR a length less than 8 characters OR more than 128 characters then they'll recieve an ALERT
-if ((incLowercaseOptions === false) && (incUppercaseOptions ===false) && (incNumericOptions === false) && (incSpecialOptions === false) {
+if ((incLowercaseOptions === false) && (incUppercaseOptions ===false) && (incNumericOptions === false) && (incSpecialOptions === false)) {
     randomOutput.innerHTML = "Invalid Entry - Please select a character length between 8 and 128 characters.";
 } 
-} else 
-{
+} else {
     randomOutput.innerHTML = "Please choose a password length between 8 and 128 characters.";
 }
 
-   
+
 //CREATE loop for the number of characters that the user will select
 for (let i = 0; i < charLength; i++) {
     let charArray = Math.floor(Math.random() * passArray.length);
-    generatedPassword.push(passArray[randomArray]);
+    generatedPassword.push(passArray[charArray]);
 };
-    return generatedPassword.join(' ');
-    };
+    return generatedPassword;
 
 
 
-
-
-
-
-
-// List out functions to get random characters from each array
-function specialRandom() {
-    var randomOutput = specialOptions[Math.floor(Math.random() * specialOptions.length)];
-    return randomOutput;
-}
-
-function uppercaseRandom() {
-    var randomOutput = uppercaseOptions[Math.floor(Math.random() * uppercaseOptions.length)];
-    return randomOutput;
-}
-
-function lowercaseRandom() {
-    var randomOutput = lowercaseOptions[Math.floor(Math.random() * lowercaseOptions.length)];
-    return randomOutput;
-}
-
-function numberRandom() {
-    var randomOutput = numericOptions[Math.floor(Math.random() * numericOptions.lenght)];
-    return randomOutput;
-}
-
-// When I click the button I'm presented with a series of prompts for password criteria
-var charLength = document.querySelector("#charLength").value;
-function choosecharLength () {
-// Ask user the number of characters they'd like to chose between 8 and 128//
-choosecharLength = prompt ("Please choose a password length between 8 and 128 characters.");
-if (charLength < 8) {
-    //Alert - too short of a password//
-    alert ("Character length less than 8 characters. Plese choose between 8 and 128 characters");
-choosecharLength();
-
-}else if (charLength > 128) {
-    //Alert - too long//
-    alert ("Password length greater than 128 characters. Please choose between 8 and 128");
-choosecharLength();
-}
-
-return charLength;
-}
-
-
-
-
-
-
-
-
-
-// Write password to the #password input
+    // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
